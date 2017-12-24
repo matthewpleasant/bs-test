7f25436f9564eb5263a65e3834ad4df8a23039a0
matthewpleasant <matthewpleasant@gmail.com>
2017-12-24 08:02:38 -0500

    Adds sending email

--- test2.py
+++ test2.py
@@ -1,4 +1,4 @@
-import bs4, requests, webbrowser, os, ui
+import bs4, requests, webbrowser, smtplib#, os, ui
 
 with open("listings.txt", "wb") as f:
 	res = requests.get("https://tampa.craigslist.org/search/sss?query=vintage%20bicycle&sort=rel").text.encode('ascII', 'ignore')
@@ -9,37 +9,38 @@ f = f.read()
 
 soup = bs4.BeautifulSoup(f, "html.parser")
 
-times = soup.find_all("time", "result-date")
-
-descriptions = soup.find_all("a", "result-title")
-
-neighborhoods = soup.find_all("span", "result-hood")
-
 area, desc, dates = [], [], []
 
-## edit loops to read like— for z in soup.find_all(...):
+for z in soup.find_all("time", "result-date"):
+	dates.append(str(z))
+	
+for d in soup.find_all("a", "result-title"):
+	desc.append(str(d))
+	
+for n in soup.find_all("span", "result-hood"):
+	area.append(str(n))
 
-with open("output.html", "w+") as o:
-	for z in times:
-		dates.append(str(z))
-	
-	for d in descriptions:
-		t = d.getText()
-		l = d["href"]
-		dl = t + " " + l + " " + "\n"
-		desc.append(dl)
-	
-	for n in neighborhoods:
-		area.append(str(z))
-	
+with open("output.html", "w+") as o:	
 	for a, b, c in zip(dates, desc, area):
 		output = a + "\n" + b + "\n" + c + "<br><br>"
 		o.write(output)
 
-file = os.path.abspath("output.html")
+#file = os.path.abspath("output.html")
 
-w = ui.WebView()
+#w = ui.WebView()
 
-w.load_url(file)
+#w.load_url(file)
 
-w.present()
+#w.present()
+
+#o = open("output.html")
+
+#info: http://naelshiab.com/tutorial-send-email-python/
+
+#server = smtplib.SMTP('smtp.gmail.com', 587)
+#server.starttls()
+#server.login("matthewpleasant@gmail.com", "Great$cott83")
+#msg = o.read()
+
+#server.sendmail("matthewpleasant@gmail.com", "matthewpleasant@gmail.com", msg)
+#server.quit()
